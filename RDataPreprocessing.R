9f20f40f3a8e380f9e652ef14fab17f357cc5c71
Johannes Lange <Johannes8lange@gmail.com>
2021-09-08 13:03:59 +0200

    Updated RDataPreprocessing file

--- RDataPreprocessing.R
+++ RDataPreprocessing.R
@@ -1,6 +1,11 @@
 setwd("C:/Users/johan/Git_Elm/data")
-Fussballer <- read.csv("fifa21_male2.csv")
+Fussballer <- read.csv("fifa21_male2.csv")#, encoding = "UTF-8")
 library(tidyverse)
+library(stringi)
+
+#Durch Löschen von Leerwerten können nicht aktive Fussballer und fehlende Werte gefiltert werden
+Fussballer <- na.omit(Fussballer)
+
 
 #Anpassen der Größe in CM
 Fussballer$Height <- (separate(data = Fussballer, col = Height, into = c("H1", "H2"), convert = TRUE) %>%
@@ -10,8 +15,42 @@ Fussballer$Height <- round(Fussballer$Height)
 #Löschen der Beschriftung am Ende von Weight (lbs) 
 Fussballer$Weight <-  parse_number(Fussballer$Weight)
 
-#Durch Löschen von Leerwerten können nicht aktive Fussballer und fehlende Werte gefiltert werden
-Fussballer <- na.omit(Fussballer)
+
+
+
+#Umwandeln des Gehaltes in einen numerischen Wert
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+Fussballer$Wage <- gsub("[â,¬]", "",Fussballer$Wage)
+#Fussballer$Wage <- parse_number(Fussballer$Wage)
+
+test1 <- substrRight(Fussballer$Wage,1)
+
+for(i in 1:length(Fussballer$Wage)){
+  if (Fussballer$Wage[i]==0){ Fussballer$Wage[i] = 0
+  }else if(grepl(test1[i],"M",fixed=TRUE )){ Fussballer$Wage[i] = parse_number(Fussballer$Wage[i])*1000
+  }else if(grepl((test1[i]),"K",fixed=TRUE )){ Fussballer$Wage[i] = parse_number(Fussballer$Wage[i])
+  }else {Fussballer$Wage[i] = parse_number(Fussballer$Wage[i])/1000
+  }
+}
+
+#Umwandeln des Wertes in einen numerischen Wert
+Fussballer$Value <- gsub("[â,¬]", "",Fussballer$Value)
+
+
+test2 <- substrRight(Fussballer$Value,1)
+
+for(i in 1:length(Fussballer$Value)){
+              if (Fussballer$Value[i]==0){ Fussballer$Value[i] = 0
+              }else if(grepl(test2[i],"M",fixed=TRUE )){ Fussballer$Value[i] = parse_number(Fussballer$Value[i])*1000
+              }else if(grepl((test2[i]),"K",fixed=TRUE )){ Fussballer$Value[i] = parse_number(Fussballer$Value[i])
+              }else {Fussballer$Value[i] = parse_number(Fussballer$Value[i])/1000
+              }
+              }
+
+      
 
 FussballerScatterplot <- data.frame(Fussballer$Name,
                                     Fussballer$Age,
@@ -27,14 +66,16 @@ FussballerScatterplot <- data.frame(Fussballer$Name,
                                     Fussballer$PAS,
                                     Fussballer$DRI,
                                     Fussballer$DEF,
-                                    Fussballer$PHY
+                                    Fussballer$PHY,
+                                    Fussballer$Wage,
+                                    Fussballer$Value
                                     )
 FussballerScatterplot <-  FussballerScatterplot[order(-FussballerScatterplot$Fussballer.OVA),]
   
 write.csv(FussballerScatterplot,"spData")
 
 
-FussballerPara <- <- data.frame(Fussballer$Name,
+FussballerPara    <- data.frame(Fussballer$Name,
                                 Fussballer$Age,
                                 Fussballer$Club,
                                 Fussballer$OVA,
@@ -49,18 +90,21 @@ FussballerPara <- <- data.frame(Fussballer$Name,
                                 Fussballer$DEF,
                                 Fussballer$PHY
 )
-  
+ 
+write.csv(FussballerPara,"pkData")
+ 
 
 FussballerBaum <-  data.frame(Fussballer$Name,
                               Fussballer$Age,
-                              Fussballer$Club)
+                              Fussballer$Club,
+                              Fussballer$BP)
 
 #Filtern von Fussballern, die nicht in den gewünschten Ligen spielen
 
 filterligen <-  c("VfL Wolfsburg", 
                   "Bayer 04 Leverkusen", 
                   "SC Freiburg", 
-                  "1. FC Köln", 
+                  "1. FC KÃ¶ln", 
                   "1. FSV Mainz 05", 
                   "1. FC Union Berlin", 
                   "Hertha BSC", 
@@ -78,23 +122,64 @@ filterligen <-  c("VfL Wolfsburg",
                   "Real Madrid",
                   "FC Barcelona",
                   "AtlÃ©tico Madrid",
-                  "Athletic Bilbao",
-                  "Betis Sevilla",
+                  "Athletic Club de Bilbao",
+                  "Real Betis",
                   "CA Osasuna",
-                  "Celta Vigo",
+                  "RC Celta",
                   "Deportivo AlavÃ©s",
                   "CÃ¡diz CF",
                   "Elche CF",
-                  "Getafe FC",
+                  "Getafe CF",
                   "Sevilla FC",
                   "Valencia CF",
                   "Villarreal CF",
                   "Granada CF",
                   "Real Sociedad",
-                  "Real Valladolid",
+                  "Real Valladolid CF",
                   "SD Eibar",
                   "SD Huesca",
-                  "Levante UD")
+                  "Levante UD",
+                  "Juventus",
+                  "Napoli",
+                  "Milan",
+                  "Inter",
+                  "Lazio",
+                  "Roma",
+                  "Atalanta",
+                  "Sassuolo",
+                  "Sampdoria",
+                  "Hellas Verona",
+                  "Genoa",
+                  "Bologna",
+                  "Fiorentina",
+                  "Udinese",
+                  "Spezia",
+                  "Cagliari",
+                  "Torino",
+                  "Benevento",
+                  "Crotone",
+                  "Parma",
+                  "Arsenal",
+                  "Aston Villa",
+                  "Brighton & Hove Albion",
+                  "Burnley",
+                  "Chelsea",
+                  "Crystal Palace",
+                  "Everton",
+                  "Fulham",
+                  "Leeds United",
+                  "Leicester City",
+                  "Liverpool",
+                  "Manchester City",
+                  "Manchester United",
+                  "Sheffield United",
+                  "Newcastle United",
+                  "Southampton",
+                  "Tottenham Hotspur",
+                  "West Bromwich Albion",
+                  "West Ham United",
+                  "Wolverhampton Wanderers"
+                  )
 
 FussballerBaum <- subset(FussballerBaum, Fussballer.Club %in% filterligen)
 write.csv(FussballerBaum,"baumData")
